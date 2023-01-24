3dacf025747e99335072e40cd3449983d6ad23d4
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-01-24 06:18:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,19 @@ def validate_api_key(api_key):
         max_tokens = st.sidebar.number_input("Number of tokens:", min_value=1, max_value=2048, value=1024)
         temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=1.0, value=0.5)
 
-    def generate_text(prompt):
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=max_tokens,
-            n=1,
-            stop=None,
-            temperature=temperature,
-        )
-        return response["choices"][0]["text"]
+def generate_text(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        temperature=temperature,
+    )
+    return response["choices"][0]["text"]
 
-    # Create a Streamlit UI for the GPT-3 playground
-    st.title("GPT-3 Playground")
-    prompt = st.text_input("Enter your prompt:")
-    st.write("Generated text:")
-    st.write(generate_text(prompt))
+# Create a Streamlit UI for the GPT-3 playground
+st.title("GPT-3 Playground")
+prompt = st.text_input("Enter your prompt:")
+st.write("Generated text:")
+st.write(generate_text(prompt))
