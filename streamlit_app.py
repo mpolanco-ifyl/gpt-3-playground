a9c90850dac436338ba58290054faca317a1464f
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-01-24 05:47:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,21 @@ import streamlit as st
 # Prompt the user for their OpenAI API key
 api_key = st.text_input("Enter your OpenAI API key:")
 
-# Use the user's API key to set the openai API key
-openai.api_key = api_key
+# Validate the API key
+def validate_api_key(api_key):
+    try:
+        openai.api_key = api_key
+        openai.Engine.list()
+        return True
+    except openai.exceptions.OpenAIError as e:
+        return False
 
-# Create variables for the number of tokens and temperature
-max_tokens = st.slider("Number of tokens:", min_value=10, max_value=4000, value=512)
-temperature = st.slider("Temperature:", min_value=0.1, max_value=1.0, value=0.5, step=0.1)
-
-# Create a function to generate GPT-3 text
+if not validate_api_key(api_key):
+    st.error("Invalid API key. Please enter a valid key.")
+else:
+    # Create variables for the number of tokens and temperature
+    st.sidebar.title("Settings")
+    
 def generate_text(prompt):
     response = openai.Completion.create(
         engine="text-davinci-003",
